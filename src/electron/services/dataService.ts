5ad00784c1640ba3c884e5b51a5226f63f3f6b87
Damjan Gjorgiev <damjangjorgiev@gmail.com>
2025-07-10 16:38:02 +0200

    configs changed

--- src/electron/services/dataService.ts
+++ src/electron/services/dataService.ts
@@ -3,17 +3,6 @@ import { ipcWebContentsSend } from "../utils/util.js";
 import { join } from "path";
 import { existsSync, readFileSync, writeFileSync } from "fs";
 
-//const POOLING_INTERVAL = 1000;
-
-// export function getMatchSeconds(window: BrowserWindow) {
-//   let seconds = 0;
-
-//   setInterval(async () => {
-//     seconds++;
-//     ipcWebContentsSend("getMatchSeconds", window.webContents, { seconds });
-//   }, POOLING_INTERVAL);
-// }
-
 export function getScoreboardState() {
   const scoreboardState: string = "scoreboardState 123";
   const teamAName: string = "Rabotnicki";
